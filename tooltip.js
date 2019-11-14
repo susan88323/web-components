b08f6a1556e2a789253486071521800b8b72eefa
Susan Wang <susanw@dev6.com>
2019-11-14 17:12:46 -0500

    Use shadown Dom to add styles

--- tooltip.js
+++ tooltip.js
@@ -3,6 +3,7 @@ class Tooltip extends HTMLElement {
         super();
         this._tooltipContainer;
         this._tooltipText = 'Some dummy tooltip text';
+        this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
@@ -15,17 +16,22 @@ class Tooltip extends HTMLElement {
         tooltipIcon.textContent = ' (?)';
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.appendChild(tooltipIcon);
+        this.shadowRoot.appendChild(tooltipIcon);
+        this.style.position = 'relative';
     }
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = this._tooltipText;
-        this.appendChild(this._tooltipContainer);
+        this._tooltipContainer.style.backgroundColor = 'black';
+        this._tooltipContainer.style.color = 'white';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = '10';
+        this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
-        this.removeChild(this._tooltipContainer);
+        this.shadowRoot.removeChild(this._tooltipContainer);
     }
 }
 
