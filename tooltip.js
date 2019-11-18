45238409172a3f4c7853b9c6bd4c4cdfa27c06b9
Susan Wang <susanw@dev6.com>
2019-11-17 21:38:41 -0500

    Style host component

--- tooltip.js
+++ tooltip.js
@@ -13,6 +13,10 @@ class Tooltip extends HTMLElement {
                     z-index: 10;
                 }
 
+                :host {
+                    background: #ccc;
+                }
+
                 .highlight {
                     background-color: red;
                 }
@@ -20,9 +24,17 @@ class Tooltip extends HTMLElement {
                 ::slotted(.highlight) {
                     border-bottom: 1px dotted red;
                 }
+
+                .icon {
+                    background: black;
+                    color: white;
+                    padding: 0.15rem 0.5rem;
+                    text-align: center;
+                    border-radius: 50%;
+                }
             </style>
             <slot>Some default</slot>
-            <span> (?)</span>
+            <span class="icon">?</span>
         `;
     }
 
