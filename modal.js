b249853f7da12f80611c3e7076f3504f98331399
Susan Wang <susanw@dev6.com>
2019-11-19 22:28:44 -0500

    Added public method and properties

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
+        this.isOpen = false;
         this.shadowRoot.innerHTML = `
             <style>
                 #backdrop {
@@ -79,20 +80,26 @@ class Modal extends HTMLElement {
 
     // We don't need to use this method to handle attribute,
     // instead, we can simply use css :host([opened])
-    // attributeChangedCallback(name, oldValue, newValue) {
-        // if (name === 'opened') {
-        //     if (this.hasAttribute('opened')) {
-        //         this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-        //         this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-        //         this.shadowRoot.querySelector('#modal').style.opacity = 1;
-        //         this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-        //     }
-        // }
-    // }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (this.hasAttribute('opened')) {
+            this.isOpen = true;
+            // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+            // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+            // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+            // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+        } else {
+            this.isOpen = false;
+        }
+    }
 
-    // static get observedAttributes() {
-    //     return ['opened'];
-    // }
+    static get observedAttributes() {
+        return ['opened'];
+    }
+
+    open() {
+        this.setAttribute('opened', '');
+        this.isOpen = true;
+    }
 }
 
 customElements.define('uc-modal', Modal);
